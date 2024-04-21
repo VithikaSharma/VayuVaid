06f02761cbb7ed5fe1357caf094a199f227cd9f8
ClearlyDhruv <dhruvnagpal1144@gmail.com>
2024-04-21 16:15:07 +0530

    removed the junk

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-window.onload = function() {                     //for website loading//
-  
+window.onload = function() {                     
     setTimeout(function() {
         document.getElementById('black-screen').style.display = 'none';
         document.getElementById('content').style.display = 'block';
     }, 2000); 
-  };
+};
+
 let navbar = document.querySelector('.navbar');
 let searchForm = document.querySelector('.search-form');
 let cartItem = document.querySelector('.cart-items-container');
@@ -12,24 +12,18 @@ let cartItem = document.querySelector('.cart-items-container');
 document.querySelector('#menu-btn').onclick = () => {
     navbar.classList.toggle('active');
     searchForm.classList.remove('active');
-    cartItem.classList.remove('active'); // Remove 'active' class from cart items when menu button is clicked
-    console.log("menu-clicked");
+    cartItem.classList.remove('active');
 }
 
 document.querySelector('#search-btn').onclick = () => {
     searchForm.classList.toggle('active');
     navbar.classList.remove('active');
-    cartItem.classList.remove('active'); // Remove 'active' class from cart items when search button is clicked
+    cartItem.classList.remove('active');
 }
-
-// document.querySelector('#cart-btn').onclick = () => {
-//     cartItem.classList.toggle('active');
-//     navbar.classList.remove('active');
-//     searchForm.classList.remove('active');
-// }
 
 window.onscroll = () => {
     navbar.classList.remove('active');
     searchForm.classList.remove('active');
-    cartItem.classList.remove('active'); // Remove 'active' class from cart items when window is scrolled
+    cartItem.classList.remove('active');
 }
+
